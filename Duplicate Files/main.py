00cb741e2526cd63063f90ccb08f769127ceb7e7
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-16 17:49:15 +0200

    Remove duplicate files in a directory

--- Duplicate Files/main.py
+++ Duplicate Files/main.py
@@ -1,15 +1,21 @@
 from importlib.resources import path
 import os
+import hashlib
+from tkinter.filedialog import askdirectory
+from tkinter import Tk
 
-directory = "/Users/justin/Documents/Python/yoco-python-course/Duplicate Files"
-file_list = os.listdir(directory)
-print(file_list)
+hash_list = []
 
-for file in file_list:
-    # Assuming duplicate files follow the format text.txt, text(1).txt
-    if "(1)" not in file:
-        continue
-    original_file_name = file.replace("(1)", "")
-    if not os.path.exists(os.path.join(directory, original_file_name)):
-        continue
-    os.remove(os.path.join(directory, file))
+Tk().withdraw()
+directory = askdirectory(title="Directory")
+
+for root, dirs, files in os.walk(directory):
+    for file_name in files:
+        file = os.path.join(root, file_name)
+        current_file_hash = hashlib.md5(
+            open(file, "rb").read()).hexdigest()  # rb opens the binary file in read format
+        if current_file_hash in hash_list:
+            os.remove(file)
+            print(f"Duplicate {file} has been removed")
+        else:
+            hash_list.append(current_file_hash)
